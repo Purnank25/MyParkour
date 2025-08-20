abc5275431608ce31d604e6c47ce49698f1c1c85
Purnank25 <purnankexcel1@gmail.com>
2025-08-20 20:09:18 +0530

    Initial commit

---
+++ Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+   [SerializeField] Transform followTarget;
+    [SerializeField] float distance = 5;
+    [SerializeField] float minVerticalAngle = -45;
+    [SerializeField] float maxVerticalAngle = 45;
+    [SerializeField]   Vector2 framingoffest;
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] bool invertx;
+    [SerializeField] bool inverty;
+    float rotaionY ;
+    float rotationX;
+    float invertxval;
+    float invertyval;
+    void Start()
+    {
+      Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        invertxval = (invertx) ? -1 : 1;
+        invertyval = (inverty) ? -1 : 1;
+        rotaionY += Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX,minVerticalAngle, maxVerticalAngle);
+        rotationX += Input.GetAxis("Mouse Y") * rotationSpeed;
+        var targetRotation = Quaternion.Euler(rotationX, rotaionY, 0);
+
+        var focusPosition = followTarget.position + new Vector3( framingoffest.x,framingoffest.y);
+        transform.position = focusPosition - targetRotation * new Vector3(0,0,distance);
+        transform.rotation = targetRotation;
+
+    }
+}
