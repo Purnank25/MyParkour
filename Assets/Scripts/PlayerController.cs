92db4fecd3afd586c9a5c2599b01dee39d08d08c
Purnank25 <purnankexcel1@gmail.com>
2025-08-21 20:21:59 +0530

    added player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float roationspeed = 500f;
     CameraController cameraController;
     Quaternion targetRotation;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
        cameraController = Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class PlayerController : MonoBehaviour
     {
        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        float moveAmount = Mathf.Abs(h) + Mathf.Abs(v);
+        float moveAmount = Mathf.Clamp01( Mathf.Abs(h) +  Mathf.Abs(v));
         var moveInput = new Vector3(h, 0, v).normalized;
         var movedir = cameraController.PlanarRotation * moveInput;
        if(moveAmount > 0)
@@ -31,5 +33,6 @@ public class PlayerController : MonoBehaviour
             targetRotation = Quaternion.LookRotation(movedir);
         }
        transform.rotation =  Quaternion.RotateTowards(transform.rotation, targetRotation,roationspeed * Time.deltaTime);
+        animator.SetFloat("moveAmount", moveAmount,0.2f,Time.deltaTime);
     }
 }
